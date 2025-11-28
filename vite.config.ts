bcc072a8631809f9f95056b10ceba3e2c1acc328
neal <neal.bristol@mediashcool.me>
2025-11-28 12:07:28 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,30 +17,15 @@ export default defineConfig({
         // Supprimer les console.log en production
         drop_console: true,
         drop_debugger: true,
-        // Optimisations supplémentaires pour la sécurité
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
-      },
-      mangle: {
-        // Obfusquer les noms de propriétés
-        properties: {
-          regex: /^_/,
-        },
       },
     },
     // Optimiser la taille des chunks
     rollupOptions: {
       output: {
         manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'react-vendor': ['react', 'react-dom'],
+          'router-vendor': ['react-router-dom'],
           'i18n-vendor': ['i18next', 'react-i18next', 'i18next-browser-languagedetector'],
-        },
-        // Exclure les fichiers sensibles du build
-        assetFileNames: (assetInfo) => {
-          // Ne pas inclure les PDFs dans le build
-          if (assetInfo.name && assetInfo.name.endsWith('.pdf')) {
-            return 'excluded/[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
         },
       },
     },
